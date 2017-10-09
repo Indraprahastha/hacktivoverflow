2ef2a9c10d3b1f4eaad8ca32138fbe5152f9eed7
Indraprahastha <indraprahastha@rocketmail.com>
2017-10-09 15:03:26 +0700

    Atlas

--- server/models/question-model.js
+++ server/models/question-model.js
@@ -1,5 +1,6 @@
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/hacktivoverflow')
+mongodb:Indraprahastha:Pancasila85@@cluster0-shard-00-00-ubshk.mongodb.net:27017,cluster0-shard-00-01-ubshk.mongodb.net:27017,cluster0-shard-00-02-ubshk.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin
+// const mongoose = require('mongoose')
+// mongoose.connect('mongodb://localhost:27017/hacktivoverflow')
 //----------------------------------------------------------
 const schema = new mongoose.Schema({
   judul:'string',
