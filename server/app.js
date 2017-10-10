05016544a16487ca9a8e3882e0ceb65e93c8d84c
Indraprahastha <indraprahastha@rocketmail.com>
2017-10-10 17:04:54 +0700

    database online

--- server/app.js
+++ server/app.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+
+const mongoose = require('mongoose')
 //----------------------------------------------------------
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(morgan('dev'))
+
+var MongoClient = require('mongodb').MongoClient;
+var uri = 'mongodb://Indraprahastha:Pancasila-85@cluster0-shard-00-00-ubshk.mongodb.net:27017,cluster0-shard-00-01-ubshk.mongodb.net:27017,cluster0-shard-00-02-ubshk.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
+
+mongoose.connect(uri);
+var db = mongoose.connection;
 //----------------------------------------------------------
 let user = require('./routers/user-router.js')
 let question = require('./routers/question-router.js')
--- server/models/question-model.js
+++ server/models/question-model.js
@@ -1,6 +1,5 @@
-mongodb:Indraprahastha:Pancasila85@@cluster0-shard-00-00-ubshk.mongodb.net:27017,cluster0-shard-00-01-ubshk.mongodb.net:27017,cluster0-shard-00-02-ubshk.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin
-// const mongoose = require('mongoose')
-// mongoose.connect('mongodb://localhost:27017/hacktivoverflow')
+const mongoose = require('mongoose')
+
 //----------------------------------------------------------
 const schema = new mongoose.Schema({
   judul:'string',
